3aa610f631e5d95f56d5cda4f14e31d3a8965778
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2022-06-29 13:18:40 +0530

    NAVBAR AND FOOTER COMPONENT DONE

---
+++ client/spotify_front/src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import SearchIcon from '@mui/icons-material/Search';
+import { Container, Wrapper, Left, Center, Right, Logo,SearchContainer, Input, MenuItem } from "../style/Navbar_Styled";
+
+
+
+const Navbar = () => {
+
+
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    
+                    <MenuItem style={{backgroundColor:"white",color:"black"}}>HOME </MenuItem>
+                  
+                   
+                    
+                </Left>
+                <Center>
+                    <Logo>SpotifyClone</Logo>
+                </Center>
+                <Right>
+                
+                   <MenuItem>REGISTER </MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <SearchContainer>
+                    <Input placeholder="search"></Input>
+                        <SearchIcon style={{color:"black",fontSize:20,paddingRight:5,cursor:"pointer"}} />
+                    </SearchContainer>
+                    
+                   
+                </Right>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default Navbar;
